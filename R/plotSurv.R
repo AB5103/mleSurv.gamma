e0e14f677978bfd4e133cb35e528a9f3cb008495
AB5103 <75606248+AB5103@users.noreply.github.com>
2022-12-09 19:12:02 +0100

    Add files via upload

--- R/plotSurv.R
+++ R/plotSurv.R
@@ -29,11 +29,12 @@
 plotSurv=function(R.model) {
  ing=R.model$sEmp[(R.model$sEmp)>0]
  lg=0.9*min(c(ing,R.model$sEst,R.model$sCor))
+ pl=0.6*max(R.model$time)
  suppressWarnings(plot(R.model$time,R.model$sEmp,type="l",xlab="time (days)",log="y",ylab="survival",
  lty=1,ylim=c(lg,1.),lwd=2))
  lines(R.model$time,R.model$sEst,col="black",lty=2,lwd=2)
  lines(R.model$time,R.model$sCor,col="black",lty=3,lwd=2)
- legend(500,1,c("empirical","estimated","corrected"),lty=1:3,bty="n",lwd=2)
+ legend(pl,1,c("empirical","estimated","corrected"),lty=1:3,bty="n",lwd=2)
  title('Empirical, estimated and corrected survivals.')
  mtext(side = 3, line=0.5,paste("Model with",R.model$dist,"baseline hazard function",sep=" "))
 }
